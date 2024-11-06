d927c9271c882a5dc6fc0ffde39f17539530f554
kirpav30 <kirpav30@gmail.com>
2024-11-06 18:09:19 +0500

    'ProjectEnd'

---
+++ backend/main/debug_helpers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+
+from .models import *
+
+
+def create_client(name, username, email, password):
+    user = User.objects.create_user(username=username, password=password, email=email)
+    clients_group = Group.objects.get(name='clients')
+    user.groups.add(clients_group)
+
+    return Client.objects.create(name=name, user=user)
+
+def create_service_company(name, description, username, email, password):
+    user = User.objects.create_user(username=username, password=password, email=email)
+    service_companies_group = Group.objects.get(name='service_companies')
+    user.groups.add(service_companies_group)
+
+    return ServiceCompany.objects.create(name=name, description=description, user=user)
+
+def create_manager(first_name, last_name, username, email, password):
+    user = User.objects.create_user(username=username, password=password, email=email)
+    managers_group = Group.objects.get(name='managers')
+    user.groups.add(managers_group)
+
+    return Manager.objects.create(user=user)
---
+++ backend/main/querysets.py
@@ -0,0 +1,162 @@
+from .models import *    
+
+def to_int(str_value, def_value):
+    if str_value.isdigit():
+        return int(str_value)
+    else:
+        return def_value
+
+def get_simple_car_queryset(request, use_filter):
+    res = Car.objects.all()
+    
+    if not use_filter:
+        return res
+
+    filter_car_num = request.GET.get('car_num', '')
+    if filter_car_num:
+        res = res.filter(car_num__icontains=filter_car_num)
+
+    return res
+
+def get_car_queryset(request, is_all, use_filter):
+    res = Car.objects.all()
+    
+    user = request.user
+
+    if not is_all:
+        if Client.is_own(user):
+            res = res.filter(client__user=user)
+        elif ServiceCompany.is_own(user):
+            res = res.filter(service_company__user=user)       
+        elif Manager.is_own(user):
+            res = res
+        elif user.is_staff:
+            res = res
+        else:
+            return Car.objects.none()
+    else:
+        if not Client.is_own(user) and not ServiceCompany.is_own(user) and not Manager.is_own(user) and not user.is_staff:
+
+            return Car.objects.none()
+
+    if not use_filter:
+        return res
+
+    filter_car_num = request.GET.get('car_num', '')
+    filter_car_model = request.GET.get('car_model', '')
+    filter_engine_model = request.GET.get('engine_model', '')
+    filter_transmission_model = request.GET.get('transmission_model', '')
+    filter_main_bridge_model = request.GET.get('main_bridge_model', '')
+    filter_steerable_bridge_model = request.GET.get('steerable_bridge_model', '')
+
+    if filter_car_num:
+        res = res.filter(car_num__icontains=filter_car_num)
+    
+    if filter_car_model:
+        filter_car_model_i = to_int(filter_car_model, -1)
+        if filter_car_model_i != -1:
+            res = res.filter(car_model=filter_car_model_i)
+    
+    if filter_engine_model:
+        filter_engine_model_i = to_int(filter_engine_model, -1)
+        if filter_engine_model_i != -1:
+            res = res.filter(engine_model=filter_engine_model_i)
+    
+    if filter_transmission_model:
+        filter_transmission_model_i = to_int(filter_transmission_model, -1)
+        if filter_transmission_model_i != -1:
+            res = res.filter(transmission_model=filter_transmission_model_i)
+    
+    if filter_main_bridge_model:
+        filter_main_bridge_model_i = to_int(filter_main_bridge_model, -1)
+        if filter_main_bridge_model_i != -1:
+            res = res.filter(main_bridge_model=filter_main_bridge_model_i)
+    
+    if filter_steerable_bridge_model:
+        filter_steerable_bridge_model_i = to_int(filter_steerable_bridge_model, -1)
+        if filter_steerable_bridge_model_i != -1:
+            res = res.filter(steerable_bridge_model=filter_steerable_bridge_model_i)
+
+    return res
+
+def get_maintenance_queryset(request, use_filter):
+    res = Maintenance.objects.all()
+
+    user = request.user
+
+    if Client.is_own(user):
+        res = res.filter(car__client__user=user)
+    elif ServiceCompany.is_own(user):
+        res = res.filter(service_company__user=user)
+    elif Manager.is_own(user):
+        res = res
+    elif user.is_staff:
+        res = res
+    else:
+        return Maintenance.objects.none()
+
+    if not use_filter:
+        return res
+
+    filter_car_num = request.GET.get('car_num', '')
+    filter_service_company__name = request.GET.get('service_company__name', '')
+    filter_type = request.GET.get('type', '')
+    
+
+    if filter_car_num:
+        res = res.filter(car__car_num__icontains=filter_car_num)
+
+    if filter_service_company__name:
+        if filter_service_company__name == "<none>":
+            res = res.filter(service_company__name__iexact=None)
+        else:
+            res = res.filter(service_company__name__icontains=filter_service_company__name)
+
+    if filter_type:
+        filter_type_i = to_int(filter_type, -1)
+        if filter_type_i != -1:
+            res = res.filter(type=filter_type_i)
+
+    return res
+
+def get_reclamation_queryset(request, use_filter):
+    res = Reclamation.objects.all()
+
+    user = request.user
+
+    if Client.is_own(user):
+        res = res.filter(car__client__user=user)
+    elif ServiceCompany.is_own(user):
+        res = res.filter(car__service_company__user=user)
+    elif Manager.is_own(user):
+        res = res
+    elif user.is_staff:
+        res = res
+    else:
+        return Reclamation.objects.none()
+
+    if not use_filter:
+        return res
+
+    filter_car_num = request.GET.get('car_num', '')
+    filter_service_company__name = request.GET.get('service_company__name', '')
+    filter_failure_node = request.GET.get('failure_node', '')
+    filter_recovery_method = request.GET.get('recovery_method', '')
+
+    if filter_car_num:
+        res = res.filter(car__car_num__icontains=filter_car_num)
+
+    if filter_service_company__name:
+        res = res.filter(car__service_company__name__icontains=filter_service_company__name)
+
+    if filter_failure_node:
+        filter_failure_node_i = to_int(filter_failure_node, -1)
+        if filter_failure_node_i != -1:
+            res = res.filter(failure_node=filter_failure_node_i)
+
+    if filter_recovery_method:
+        filter_recovery_method_i = to_int(filter_recovery_method, -1)
+        if filter_recovery_method_i != -1:
+            res = res.filter(recovery_method=filter_recovery_method_i)
+
+    return res
